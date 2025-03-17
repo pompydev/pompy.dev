139c36a85091e94148dd7990d06de4de8260db90
pompydev <pompydev@proton.me>
2025-03-17 23:07:23 +0900

    refactor out ThemeController

---
+++ apps/rank/components/ThemeController.vue
@@ -0,0 +1,16 @@
+<script setup>
+const colorMode = useColorMode()
+</script>
+
+<template>
+    <p class="p-4 pb-2">
+        <select
+            v-model="colorMode.preference"
+            class="h-8 w-24 border dark:border-gray-700 dark:bg-gray-900 dark:text-white"
+        >
+            <option value="system">System</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+        </select>
+    </p>
+</template>
