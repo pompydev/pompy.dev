eb3eb66d73aad36e44f13f2d2ca22d0ca3c9caf2
pompydev <pompydev@proton.me>
2025-03-18 06:26:06 +0900

    add custom error page to apps/rank

---
+++ apps/rank/error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app"
+
+const props = defineProps({
+    error: Object as () => NuxtError,
+})
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <h2 class="text-8xl font-black">{{ $props.error?.statusCode }}</h2>
+        <p>{{ $props.error?.statusMessage }}</p>
+        <NuxtLink href="/" class="underline">bring me home!</NuxtLink>
+    </div>
+</template>
