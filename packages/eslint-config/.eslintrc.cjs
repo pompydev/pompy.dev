5c17ce60131808e531e5f380fa2e9255d39e4fd7
pompydev <pompydev@proton.me>
2025-03-18 13:14:47 +0900

    remove prettier business from eslint business

--- packages/eslint-config/.eslintrc.cjs
+++ packages/eslint-config/.eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
         "turbo",
     ],
     ignorePatterns: ["dist/", "build/"],
