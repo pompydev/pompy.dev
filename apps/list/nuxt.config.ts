057214d80d7226f2879256a42bea4c6476e1b179
pompydev <pompydev@proton.me>
2025-03-20 14:36:29 +0900

    deploy list.pompy.dev to huxthub

--- apps/list/nuxt.config.ts
+++ apps/list/nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
         "@nuxtjs/tailwindcss",
         "@nuxtjs/google-fonts",
         "@nuxt/icon",
+        "@nuxthub/core",
     ],
     app: {
         head: {
