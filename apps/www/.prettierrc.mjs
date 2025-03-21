e81efcdb47d837c9f743c55fec16a4cd0ce89f2b
pompydev <pompydev@proton.me>
2025-03-21 12:36:11 +0900

    chore: fix prettier plugin

--- apps/www/.prettierrc.mjs
+++ apps/www/.prettierrc.mjs
@@ -1,11 +1,11 @@
-const sharedConfig = import("@pompydev/prettier-config")
+import pompydevPrettierConfig from "@pompydev/prettier-config" with { type: "json" }
 
 /** @type {import("prettier").Options} */
-module.exports = {
-    ...sharedConfig,
+export default {
+    ...pompydevPrettierConfig,
     plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
     overrides: [
-        ...sharedConfig.overrides,
+        ...pompydevPrettierConfig.overrides,
         { files: "*.svelte", options: { parser: "svelte" } },
     ],
 }
