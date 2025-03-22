de23c9d168ef1bdc6f6959b39e8912bee197822c
developomp <developomp@protonmail.com>
2025-03-23 08:38:31 +0900

    feat(blog): remove non-conforming attributes
    check https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -56,11 +56,7 @@ export default function RootLayout({
     return (
         <html lang="en" className="dark">
             <head>
-                <link
-                    rel="shortcut icon"
-                    type="image/svg+xml"
-                    href="/favicon.svg"
-                />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
                 <meta name="theme-color" content="#000000" />
                 <script
                     defer
